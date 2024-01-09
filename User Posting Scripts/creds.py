1beec847259d1a5222bad0987b8d67ad978c6d9e
Kannan <kthuvakaran43@gamil.com>
2024-01-09 20:14:47 +0000

    add creds

---
+++ User Posting Scripts/creds.py
@@ -0,0 +1,12 @@
+dag_credentials = {"databricks_conn_id": 'databricks_default',
+                   "existing_cluster_id": '1108-162752-8okw8dgg'}
+
+RDS_database = {"HOST": "pinterestdbreadonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com",
+        "USER": 'project_user',
+        "PASSWORD": ':t%;yCY3Yjg',
+        "DATABASE": 'pinterest_data',
+        "PORT": 3306}
+
+invoke_url = {"batch_url": "https://nv2gj4px8k.execute-api.us-east-1.amazonaws.com/beta/topics/",
+              "stream_url": "https://nv2gj4px8k.execute-api.us-east-1.amazonaws.com/test-streaming/streams/"}
+
