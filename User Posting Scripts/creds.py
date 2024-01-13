06f8a559903849f170fbbae3fcbf1718dbefbd77
Kannan Thuvakaran <52046129+KannanThuvakaran@users.noreply.github.com>
2024-01-13 01:17:43 +0000

    Update creds.py

--- User Posting Scripts/creds.py
+++ User Posting Scripts/creds.py
@@ -1,12 +1,12 @@
-dag_credentials = {"databricks_conn_id": 'databricks_default',
-                   "existing_cluster_id": '1108-162752-8okw8dgg'}
+dag_credentials = {"databricks_conn_id": '',
+                   "existing_cluster_id": ''}
 
-RDS_database = {"HOST": "pinterestdbreadonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com",
-        "USER": 'project_user',
-        "PASSWORD": ':t%;yCY3Yjg',
-        "DATABASE": 'pinterest_data',
-        "PORT": 3306}
+RDS_database = {"HOST": "",
+        "USER": '',
+        "PASSWORD": '',
+        "DATABASE": '',
+        "PORT": }
 
-invoke_url = {"batch_url": "https://nv2gj4px8k.execute-api.us-east-1.amazonaws.com/beta/topics/",
-              "stream_url": "https://nv2gj4px8k.execute-api.us-east-1.amazonaws.com/test-streaming/streams/"}
+invoke_url = {"batch_url": "",
+              "stream_url": ""}
 
